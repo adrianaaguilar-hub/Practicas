e5fa93e7543da6875e8fc7c021140198059aef88
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-02-22 15:23:21 +0100

    Agregar clases Persona y Estudiante para práctica de POO, junto con clases Estudiante y Calificaciones, aun no comenzado

---
+++ Programación2/POO/Persona.java
@@ -0,0 +1,16 @@
+package Programación2.POO;
+
+public class Persona {
+
+    String name;
+    int age;
+
+    public Persona(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Hola() {
+        System.out.println("Mi nombre es " + name + " y tengo " + age + " años");
+    }
+}
