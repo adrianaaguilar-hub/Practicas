91875309e0a8820cd13b13d3b8cdbe897f4799bd
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-03-08 22:25:35 +0100

    repasé hoy ⭐

--- Programación2/src/vistas/Intervalo.java
+++ Programación2/src/vistas/Intervalo.java
@@ -1,21 +1,46 @@
 package Programación2.src.vistas;
 
  class Intervalo {
+
+    private double inferior;
+    private double superior;
+
+    public Intervalo (double inferior, double superior) {
+        this.inferior = inferior;
+        this.superior = superior;
+    }
     
-    public double longitud ();
+    public double longitud () {
+        return superior - inferior;
+    }
 
-    public void desplazar(double desplazamiento);
 
-    public Intervalo desplazado(double desplazamiento);
+    public void desplazar(double desplazamiento) {
+        inferior = inferior + desplazamiento;
+        superior = superior + desplazamiento;
+    }
+
+
+    public Intervalo desplazado(double desplazamiento){
+        return new Intervalo (inferior + desplazamiento, superior + desplazamiento);
+    }
     
-    public boolean incluye (double punto);
+    public boolean incluye (double punto) {
+        return (punto >= inferior && punto <= superior);
+    }
 
-    public boolean igual (Intervalo intervalo);
+    public boolean igual (Intervalo intervalo) {
+        return intervalo.inferior == inferior && intervalo.superior == superior;
+    }
 
-    public boolean esValido(INtervalo intervalo);
+    public double puntoMedio () {
+        return media() + inferior;
+    }
 
-    public double puntoMedio (); 
-
-    public double media();
+    public double media() {
+        return longitud() / 2;
+    }
 
 }
+
+// Repasé hoy  ⭐
