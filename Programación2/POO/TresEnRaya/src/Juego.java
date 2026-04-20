4973542aa8523eb1e000e4c20642fd8e4b5c9601
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-04-20 23:45:21 +0200

    Feat: modificacion vista publica

--- Programación2/POO/TresEnRaya/src/Juego.java
+++ Programación2/POO/TresEnRaya/src/Juego.java
@@ -1,31 +1,43 @@
 package Programación2.TresEnRaya.src;
 
 public class Juego {
-      
-    public Juego () {}
     
-    public static void main(String[] args) {
-         do {
+    public Juego () {}
+    private Tablero tablero;
+    private Turno turno;
+    private Jugadores [] jugadores;
+    
+    public Juego () {
+        this.tablero = new Tablero();
+        this.turno = new Turno ();
+        this.jugadores = new Jugadores();
+    }
+
+    do {
         limpiarPantalla();
         iniciarJuego();
         mostrarJuego();
         jugarPartida();
         gestionarTurno();
-    } while (!Tablero.hayTresEnRaya());
-    }
+    } while (Tablero tablero = );
+    
    
 
-    public void mostrarJuego (Tablero tablero, Ficha ficha) {}
+    private void mostrarJuego (Tablero tablero, Ficha ficha) {}
 
-    public void iniciarJuego () {}
+    private void iniciarJuego (Tablero tablero, Ficha ficha) {}
 
-    public void jugarPartida () {}
+    private void jugarPartida () {}
 
-    public void gestionarTurno (Turno turno) {
+    private void gestionarTurno (Turno turno) {
         Turno.turnoJugadorX ();
         Turno.turnoJugador0 ();
     }
 
-    public void limpiarPantalla () {}
+    private void limpiarPantalla () {}
+
+    private void moverFicha() {}
+
+    private boolean
 
 }
--- Programación2/POO/TresEnRaya/src/Jugador.java
+++ Programación2/POO/TresEnRaya/src/Jugador.java
@@ -1,9 +1,15 @@
 package Programación2.TresEnRaya.src;
 
 public class Jugador {
-    
-    public Jugador () {}
+    private String nombre;
+    private String ficha;
+    public Jugador (String nombre, String ficha) {
+        this.nombre = nombre;
+        this.ficha = ficha;
+    }
 
-    
+    public void jugarTurno (Turno turno) {
+        Turno turno = new turno
+    }
 
 }
