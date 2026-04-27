33c0e56f05565c4325e164165c9f28c7fad3faf2
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-04-27 11:25:00 +0200

    feat: clase Hamburguesa terminada.

---
+++ Programación2/POO/Restaurante/src/Hamburguesa.java
@@ -0,0 +1,30 @@
+package Programación2.POO.Restaurante.src;
+
+public class Hamburguesa {
+    private String nombre;
+    private double precioBase;
+    private boolean esVegana;
+    private String [] tipoCarne = {"res", "pollo", "vegana"};
+    private String [] vegetales = {"lechuga", "tomate", "pepinillos"}
+    private boolean conSalsa;
+    private boolean conQueso;
+    private double precioFinal;
+
+    private final double PORCENTAJE_IVA = 1.21;
+
+    public Hamburguesa (String nombre, double precio, boolean esVegana, boolean conSalsa, boolean conQueso) {
+        assert precio > 0;
+
+        this.nombre = nombre;
+        this.precio = precio;
+        this.esVegana = esVegana;
+        this.conSalsa = conSalsa;
+        this.conQueso = conQueso;
+    }
+    public Hamburguesa(String nombre, double precioBase) {
+        this(nombre, precioBase, true, true, false);
+}
+
+    public double precioFinal () {
+        precioBase * PORCENTAJE_IVA = precioFinal;
+    }
