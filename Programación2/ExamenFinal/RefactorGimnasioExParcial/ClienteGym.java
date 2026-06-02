a995652f36a81b9e6f16482e76d1b1263bfe7bc6
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-06-02 16:15:55 +0200

    Refactor: se nombraron variables para mejorar lectura de codigo

--- Programación2/ExamenFinal/RefactorGimnasioExParcial/ClienteGym.java
+++ Programación2/ExamenFinal/RefactorGimnasioExParcial/ClienteGym.java
@@ -1,15 +1,19 @@
 package Programación2.ExamenFinal.RefactorGimnasioExParcial;
 
 public class ClienteGym {
+    private boolean estaActivo = true;
+    private boolean estaInactivo = false;
+    private int aforo14 = 14;
+    private int aforo15 = 15;
      public static void main(String[] args) {
 
         Gimnasio miGym = new Gimnasio("Fitness Park");
 
-        Socio socio1 = new Socio("Adriana Aguilar", "12345678A", true);
-        Socio socio2 = new Socio("Samuel Herrera", "87654321B", true);
+        Socio socio1 = new Socio("Adriana Aguilar", "12345678A", estaActivo);
+        Socio socio2 = new Socio("Samuel Herrera", "87654321B", estaActivo);
 
-        Actividad spinning = new Actividad("Spinning", "Adriana", 14);
-        Actividad yoga = new Actividad("Yoga", "Samuel", 15);
+        Actividad spinning = new Actividad("Spinning", "Adriana", aforo14);
+        Actividad yoga = new Actividad("Yoga", "Samuel", aforo15);
 
         miGym.inscribirSocio(socio1);
         miGym.inscribirSocio(socio2);
